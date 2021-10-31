3cf0ee8d36fda9f2c45c5df9f841af24e0b75245
absolutedevelop <mathalefortunate5@gmail.com>
2021-10-31 18:58:33 +0200

    added chnages

--- main.go
+++ main.go
@@ -6,8 +6,22 @@ func main(){
 
 	//creat integers 
 	var age int = 23
+	fmt.Println("Your age is", age )
 
-	fmt.Println("Your age is : ", age )
+	//changing a value
+	age = 24
+	fmt.Println("Your new age is", age)
+
+	//create a string 
+	var name string = "Fortunate"
+	fmt.Println("Your name is", name)
+
+	//create a float 
+	var height float64 = 1.63
+	fmt.Println("Your height is" , height)
+
+	var isHappy bool = true
+	fmt.Println("You are happy" , isHappy)
 
 
 }
